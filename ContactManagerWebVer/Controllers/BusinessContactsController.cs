609b420071e6ace934736775592dffebed8aabb3
Raven Chen <chenr21@cucollege.coventry.ac.uk>
2020-04-07 22:35:51 +0100

    v0.3 Scaffolded Business Contact

---
+++ ContactManagerWebVer/Controllers/BusinessContactsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactManagerWebVer.Data;
+using HarrisContactWebVer.Models;
+
+namespace ContactManagerWebVer.Controllers
+{
+    public class BusinessContactsController : Controller
+    {
+        private readonly ContactDbContext _context;
+
+        public BusinessContactsController(ContactDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BusinessContacts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BusinessContact.ToListAsync());
+        }
+
+        // GET: BusinessContacts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessContact = await _context.BusinessContact
+                .FirstOrDefaultAsync(m => m.BusinessContactID == id);
+            if (businessContact == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessContact);
+        }
+
+        // GET: BusinessContacts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BusinessContacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BusinessContactID,BFname,BLname,BEmail,BusTel,BAddr1,BAddr2,BAddr3,BPostcode,BCity")] BusinessContact businessContact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(businessContact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(businessContact);
+        }
+
+        // GET: BusinessContacts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessContact = await _context.BusinessContact.FindAsync(id);
+            if (businessContact == null)
+            {
+                return NotFound();
+            }
+            return View(businessContact);
+        }
+
+        // POST: BusinessContacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BusinessContactID,BFname,BLname,BEmail,BusTel,BAddr1,BAddr2,BAddr3,BPostcode,BCity")] BusinessContact businessContact)
+        {
+            if (id != businessContact.BusinessContactID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(businessContact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BusinessContactExists(businessContact.BusinessContactID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(businessContact);
+        }
+
+        // GET: BusinessContacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessContact = await _context.BusinessContact
+                .FirstOrDefaultAsync(m => m.BusinessContactID == id);
+            if (businessContact == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessContact);
+        }
+
+        // POST: BusinessContacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var businessContact = await _context.BusinessContact.FindAsync(id);
+            _context.BusinessContact.Remove(businessContact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BusinessContactExists(int id)
+        {
+            return _context.BusinessContact.Any(e => e.BusinessContactID == id);
+        }
+    }
+}
